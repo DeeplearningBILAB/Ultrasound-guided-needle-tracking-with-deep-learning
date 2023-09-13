aed37b3a467b589cdc15e0aab60c0e5ad8d1fa71
Biomedical Imaging Laboratory@ISU <99789227+DeeplearningBILAB@users.noreply.github.com>
2023-09-13 12:03:22 -0700

    Update config_test.py

--- config_test.py
+++ config_test.py
@@ -1,5 +1,5 @@
 
 
-TEST_FILENAME = r"D:\PA_US_new\Dataset_train\test\test_set.npz"
-TEST_DIR = r"D:\PA_US_pytorch\Results\test"
-BEST_MODEL = r"D:\PA_US_pytorch\model\best_metric_model.pth"
+TEST_FILENAME = r"D:\PA_US\test\test_set.npz"
+TEST_DIR = r"D:\PA_US\Results\test"
+BEST_MODEL = r"D:\PA_US\model\best_metric_model.pth"
